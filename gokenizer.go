2e7e4508467409bf0fd81972b13d62bf996e5473
Jason McVetta <jason.mcvetta@gmail.com>
2012-03-30 22:18:04 -0700

    Add GPL license text.

--- gokenizer.go
+++ gokenizer.go
@@ -7,6 +7,20 @@
 @copyright: (c) 2012 Jason McVetta
 @license: GPL v3 - http://www.gnu.org/copyleft/gpl.html
 
+********************************************************************************
+Gokenizer is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+Gokenizer is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Gokenizer.  If not, see <http://www.gnu.org/licenses/>.
+********************************************************************************
+
 */
 
 package main
@@ -31,7 +45,7 @@ const (
 	success        = "Success"
 )
 
-var TokenNotFound = errors.New("Foobar")
+var TokenNotFound = errors.New("Token Not Found")
 
 type TokenizeRequest struct {
 	ReqId string            // Request ID string will be returned unchanged with the response to this request
