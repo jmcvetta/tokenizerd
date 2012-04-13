3a53b01f70d4b2ff04197bd5e93a861c2ec1a411
Jason McVetta <jason.mcvetta@gmail.com>
2012-04-13 04:40:41 -0700

    Move API functionality to separate module.

---
+++ src/api/api.go
@@ -0,0 +1,157 @@
+/*
+                                   Gokenizer
+                               A Data Tokenizer
+API
+
+
+@author: Jason McVetta <jason.mcvetta@gmail.com>
+@copyright: (c) 2012 Jason McVetta
+@license: GPL v3 - http://www.gnu.org/copyleft/gpl.html
+
+********************************************************************************
+This file is part of Gokenizer.
+
+Gokenizer is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+Gokenizer is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Gokenizer.  If not, see <http://www.gnu.org/licenses/>.
+********************************************************************************
+
+*/
+
+package api
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"encoding/json"
+	"io"
+	"log"
+	"tokenizer"
+)
+
+// Maybe these should be more similar to HTTP response codes.
+const (
+	invalidRequest = "Invalid Request"
+	success        = "Success"
+)
+
+type JsonTokenizeRequest struct {
+	ReqId string            // Request ID string will be returned unchanged with the response to this request
+	Data  map[string]string // Maps fieldnames to text
+}
+
+type TokenizeReponse struct {
+	ReqId  string            // Request ID string from orginating JsonTokenizeRequest
+	Status string            // Status code
+	Error  string            // Error message if any
+	Data   map[string]string // Maps fieldnames to token strings
+}
+
+type DetokenizeRequest struct {
+	ReqId string // Request ID string will be returned unchanged with the response to this request
+	Data  map[string]string
+}
+
+type foundToken struct {
+	// Is it really pointful to return the token?
+	Token string // The token we looked up
+	Found bool   // Was the token found in the database?
+	Text  string // The text it represents, if found
+}
+
+type DetokenizeReponse struct {
+	ReqId  string                // Request ID string from orginating JsonTokenizeRequest
+	Status string                // Status code
+	Error  string                // Error message if any
+	Data   map[string]foundToken // Maps fieldnames to foundToken instances
+}
+
+type wsHandler func(ws *websocket.Conn)
+
+func HandlerTokenize(t tokenizer.Tokenizer) wsHandler {
+	return func(ws *websocket.Conn) {
+		log.Println("New websocket connection")
+		log.Println("    Location:  ", ws.Config().Location)
+		log.Println("    Origin:    ", ws.Config().Origin)
+		log.Println("    Protocol:  ", ws.Config().Protocol)
+		dec := json.NewDecoder(ws)
+		enc := json.NewEncoder(ws)
+		for {
+			var request JsonTokenizeRequest
+			// Read one request from the socket and attempt to decode
+			switch err := dec.Decode(&request); true {
+			case err == io.EOF:
+				log.Println("Websocket disconnecting")
+				return
+			case err != nil:
+				// Request could not be decoded - return error
+				response := TokenizeReponse{Status: invalidRequest, Error: err.Error()}
+				enc.Encode(&response)
+				log.Println("Invalid request - websocket disconnecting")
+				return
+			}
+			data := make(map[string]string)
+			for fieldname, text := range request.Data {
+				data[fieldname] = t.Tokenize(text)
+			}
+			response := TokenizeReponse{
+				ReqId:  request.ReqId,
+				Status: success,
+				Data:   data,
+			}
+			enc.Encode(response)
+		}
+	}
+}
+
+// A websocket handler for detokenization
+func HandlerDetokenize(t tokenizer.Tokenizer) wsHandler {
+	return func(ws *websocket.Conn) {
+		dec := json.NewDecoder(ws)
+		enc := json.NewEncoder(ws)
+		for {
+			var request DetokenizeRequest
+			// Read one request from the socket and attempt to decode
+			switch err := dec.Decode(&request); true {
+			case err == io.EOF:
+				log.Println("Websocket disconnecting")
+				return
+			case err != nil:
+				// Request could not be decoded - return error
+				response := DetokenizeReponse{Status: invalidRequest, Error: err.Error()}
+				enc.Encode(&response)
+				return
+			}
+			data := make(map[string]foundToken)
+			for fieldname, token := range request.Data {
+				ft := foundToken{
+					Token: token,
+				}
+				text, err := t.Detokenize(token)
+				switch {
+				case nil == err:
+					ft.Text = text
+					ft.Found = true
+				case err == tokenizer.TokenNotFound:
+					ft.Found = false
+				case err != nil:
+					log.Panic(err)
+				}
+				data[fieldname] = ft
+			}
+			response := DetokenizeReponse{
+				ReqId:  request.ReqId,
+				Status: success,
+				Data:   data,
+			}
+			enc.Encode(response)
+		}
+	}
+}
--- src/api/api_test.go
+++ src/api/api_test.go
@@ -1,6 +1,8 @@
 /*
                                    Gokenizer
-                               A Data Tokenizer
+                                  Test Suite
+
+NOTE: Gokenizer application must be running in order to run tests.
 
 
 @author: Jason McVetta <jason.mcvetta@gmail.com>
@@ -8,6 +10,8 @@
 @license: GPL v3 - http://www.gnu.org/copyleft/gpl.html
 
 ********************************************************************************
+This file is part of Gokenizer.
+
 Gokenizer is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
 Foundation, either version 3 of the License, or (at your option) any later
@@ -23,41 +27,45 @@ Gokenizer.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-package main
+package api
 
 import (
 	"code.google.com/p/go.net/websocket"
-	"flag"
-	"launchpad.net/mgo"
+	"encoding/json"
+	"github.com/jmcvetta/goutil"
 	"log"
-	"net/http"
-	"tokenizer"
-	"api"
+	"testing"
 )
 
-func main() {
-	log.SetFlags(log.Ltime | log.Lshortfile)
-	//
-	// Parse command line
-	//
-	listenUrl := flag.String("url", "localhost:3000", "Host/port on which to run websocket listener")
-	mongoUrl := flag.String("mongo", "localhost", "URL of MongoDB server")
-	flag.Parse()
-	//
-	// Setup database connection
-	//
-	log.Println("Connecting to MongoDB on", *mongoUrl)
-	session, err := mgo.Dial(*mongoUrl)
+var tokenizeReq = `
+{
+    "ReqId": "an arbitrary string identifying this request",
+    "Data": {
+        "fieldname1": "fieldvalue1",
+        "field name 2": "field  value 2"
+    }
+}
+`
+
+func getWebsocket(t *testing.T) *websocket.Conn {
+	origin := "http://localhost/"
+	url := "ws://localhost:3000/v1/tokenize"
+	ws, err := websocket.Dial(url, "", origin)
 	if err != nil {
-		log.Fatalln(err)
+		msg := "Could not conect to websocket.  Is Gokenizer running?"
+		log.Println(msg)
+		t.Fatal(err)
 	}
-	db := session.DB("gokenizer")
+	return ws
+}
+
+func runServer() error {
 	//
-	// Initialize tokenizer
+	// Use a fake tokenizer since we are only interested in testing the API.
 	//
-	t := tokenizer.NewMongoTokenizer(db)
-	tok := api.HandlerTokenize(t)
-	detok := api.HandlerDetokenize(t)
+	fake := FakeTokenizer{}
+	tok := HandlerTokenize(fake)
+	detok := HandlerDetokenize(t)
 	//
 	// Start websocket listener
 	//
@@ -65,8 +73,28 @@ func main() {
 	http.Handle("/v1/tokenize", websocket.Handler(tok))
 	http.Handle("/v1/detokenize", websocket.Handler(detok))
 	// listenUrl := "heliotropi.cc:3000"
-	err = http.ListenAndServe(*listenUrl, nil)
+	err = http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatalln("ListenAndServe: " + err.Error())
 	}
 }
+
+// A simple-minded fake tokenizer for testing.  Original string and token are 
+// always identical, so no storage or logic is required.
+type FakeTokenizer struct{}
+
+func (f FakeTokenizer) Tokenize(s string) string {
+	return s
+}
+
+func (f FakeTokenizer) Detokenize(s string) (string, error) {
+	return s, nil
+}
+
+
+func TestTokenizeApi(t *testing.T) {
+}
+
+func TestDetokenizeApi(t *testing.T) {
+}
+
