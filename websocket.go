9080259fdcb7aca96a2750f99dedb9d6bd843fa6
Jason McVetta <jason.mcvetta@gmail.com>
2012-04-22 07:40:56 -0700

    fix capitalization

--- websocket.go
+++ websocket.go
@@ -1,4 +1,4 @@
-// Copyright 2012 Jason McVetta.  This is free software, released under the 
+// Copyright 2012 Jason McVetta.  This is Free Software, released under the 
 // terms of the GNU Public License version 3.
 
 package main
